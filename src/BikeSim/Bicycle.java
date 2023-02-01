171657b630779e43eb2c8c6932af10aa6ac5474a
lainanguyen <lainanguyen7@gmail.com>
2023-02-01 08:33:08 -0500

    add Bicycle methods

--- src/BikeSim/Bicycle.java
+++ src/BikeSim/Bicycle.java
@@ -1,4 +1,23 @@
 package BikeSim;
 
 public class Bicycle {
+    // Blueprint to create a Bicycle object
+
+    // Attributes
+    private String color;
+    private int gear;
+    private double maxVelocity;
+
+    // Constructor: what gets called to build the Bike
+    Bicycle() {
+        color = "Pink";
+        gear = 1;
+        maxVelocity = 20;
+    }
+
+    // Methods
+    public void brake(){
+        System.out.println("I'm braking...");
+    }
+
 }
---
+++ src/BikeSim/Level1.java
@@ -0,0 +1,13 @@
+package BikeSim;
+
+public class Level1 {
+    public static void main(String[] args) {
+        System.out.println("----------Loaded Level 1----------");
+
+        // Creates a new Bicycle object from Bicycle class
+        Bicycle defaultBike = new Bicycle();
+
+        defaultBike.brake();
+
+    }
+}

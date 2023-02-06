6aa85ef988a690bb0466bed410ad0820cb8833c5
lainanguyen <lainanguyen7@gmail.com>
2023-02-06 08:14:06 -0500

    import files from elearning

---
+++ src/PracticeQuestions/Practice1.java
@@ -0,0 +1,26 @@
+package PracticeQuestions;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        //call methods here to test if they work
+
+    }
+
+    static double volumeOfCylinder(double r, double h) {
+        //Use the parameters r and h, and calculate
+        //the volume of that cylinder. return the answer
+
+    }
+
+    static double newAverage(int[] grades) {
+        //Remove the lowest grade in the array, and then return the new average
+
+    }
+
+    static int vowelCount(String s) {
+        //Use the parameter s, and calculate how many vowels (aeiou)
+        //are contained in the string. Return that number.
+
+    }
+
+}

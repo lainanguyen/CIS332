0297aa8e52b4ff29fc88849df5369b8c9bfa68a5
lainanguyen <lainanguyen7@gmail.com>
2023-02-08 08:58:46 -0500

    generate test for homework

---
+++ src/Test/Homework/HWset2Test.java
@@ -0,0 +1,13 @@
+package Homework;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HWset2Test {
+
+    @org.junit.jupiter.api.Test
+    void didIPass() {
+//        assertEquals(false, didIPass(true, 0));
+    }
+
+
+}
